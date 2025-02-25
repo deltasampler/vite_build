756dbca07baf8d49911f5b08d42b26854ecc45e2
deltasampler <deltasampler@gmail.com>
2025-02-25 19:21:24 +0200

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import {defineConfig} from "vite";
+import path from "path";
+
+export default defineConfig({
+    resolve: {
+        alias: {
+            '@engine': path.resolve(__dirname, "./modules/engine/source"),
+            '@examples': path.resolve(__dirname, "./modules/examples/source"),
+            '@cl': path.resolve(__dirname, "./modules/cl/source"),
+            '@gui': path.resolve(__dirname, "./modules/gui/source"),
+            '@panel_templater': path.resolve(__dirname, "./modules/panel_templater/source")
+        }
+    }
+});
