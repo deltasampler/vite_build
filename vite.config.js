48d0dc5134f86a1bb6ff08219c55bde1ab2102ad
deltasampler <deltasampler@gmail.com>
2025-02-27 16:02:17 +0200

    add config paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import {defineConfig} from "vite";
+import path from "path";
 
 export default defineConfig({
     resolve: {
-        alias: {}
+        alias: {
+            '@engine': path.resolve(__dirname, "./modules/engine/source"),
+            '@examples': path.resolve(__dirname, "./modules/examples/source"),
+            '@cl': path.resolve(__dirname, "./modules/cl/source"),
+            '@gui': path.resolve(__dirname, "./modules/gui/source"),
+            '@panel_templater': path.resolve(__dirname, "./modules/panel_templater/source"),
+            '@square': path.resolve(__dirname, "./modules/square/source"),
+            '@turmite': path.resolve(__dirname, "./modules/turmite/source")
+        }
     }
 });
